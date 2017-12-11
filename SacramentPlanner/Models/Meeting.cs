9f0c690bd5371c552245433c3dc370c2acdf8032
stuart pennington <slp423@comcast.net>
2017-12-11 11:56:44 -0700

    Changed the _Layout.cshtml to access the views. updated database and finished the CRUD for both controllers

--- SacramentPlanner/Models/Meeting.cs
+++ SacramentPlanner/Models/Meeting.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 
 namespace SacramentPlanner.Models
 {
+    public enum Speakers
+    {
+        Bishop,
+        First_Councilor,
+        Second_Councilor,
+        Other
+    }
+
     public partial class Meeting
     {
         public int MeetingId { get; set; }
